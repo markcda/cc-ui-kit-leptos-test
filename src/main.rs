f7a903125e65fd72f082a3490cb5d2137d4351e3
Klimenty Titov <aclo.create@gmail.com>
2025-01-23 12:13:44 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,61 @@
 use cc_ui_kit::prelude::*;
 
+use log::info;
+use lucide_leptos::ArrowRightFromLine;
+use thaw::*;
+
 fn main() {
-  leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+  console_error_panic_hook::set_once();
+  #[cfg(debug_assertions)]
+  console_log::init_with_level(log::Level::Debug).unwrap();
+  #[cfg(not(debug_assertions))]
+  console_log::init_with_level(log::Level::Info).unwrap();
+  info!("starting app");
+  leptos::mount::mount_to_body(App)
+}
+
+#[component]
+fn App() -> impl IntoView {
+  let open = RwSignal::new(false);
+  let open_f = move || open.set(true);
+  let theme = RwSignal::new({
+    let mut theme = Theme::light();
+    theme.color.color_brand_background = "#17171a".to_string();
+    theme.color.color_brand_background_hover = "#2c2c32".to_string();
+    theme.color.color_brand_background_pressed = "#2c2c32".to_string();
+    theme
+  });
+
+  view! {
+    <ConfigProvider theme>
+      <OverlayDrawer open position=DrawerPosition::Left>
+        <DrawerHeader>
+          <DrawerHeaderTitle>
+            <DrawerHeaderTitleAction slot>
+              <Button appearance=ButtonAppearance::Subtle on_click=move |_| open.set(false)>
+                "x"
+              </Button>
+            </DrawerHeaderTitleAction>
+            "Default Drawer"
+          </DrawerHeaderTitle>
+        </DrawerHeader>
+        <DrawerBody>
+          <p>"Drawer content"</p>
+        </DrawerBody>
+      </OverlayDrawer>
+      <div class="ml-4 mr-4 mt-4">
+        <Button appearance=ButtonAppearance::Primary on_click=move |_| open_f()>
+          <ArrowRightFromLine color="white" size=24 stroke_width=2 />
+          <p class="ml-2">"Hello there!"</p>
+        </Button>
+      </div>
+      <Skeleton>
+        <div class="p-4 space-y-2">
+          <SkeletonItem />
+          <SkeletonItem />
+          <SkeletonItem />
+        </div>
+      </Skeleton>
+    </ConfigProvider>
+  }
 }
